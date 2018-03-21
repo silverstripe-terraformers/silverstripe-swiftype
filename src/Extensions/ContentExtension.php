1d8f4bb4bb48bf95c14d7fc94b26529b1eec620b
Charlie Bergthaler <ichaberg@gmail.com>
2018-03-21 15:55:03 +1300

    Change ContentExtension including metatags config to include namespace and allow FQ class names.

--- src/Extensions/ContentExtension.php
+++ src/Extensions/ContentExtension.php
@@ -3,47 +3,54 @@ namespace Ichaber\SSSwiftype\Extensions;
 
 
 use SilverStripe\Core\Extension;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Config\Configurable;
 
 class ContentExtension extends Extension
 {
-//    protected $swiftypeMetaTags = array(
-//        'Body',
-//        'PublishedAt',
-//        'Title',
-//        'UpdatedAt',
-//        'URL',
-//    );
-    protected $swiftypeMetaTags = [];
+    use Configurable;
+
+    protected $namespace = '';
+    protected $metaClasses = [];
 
     public function __construct()
     {
         parent::__construct();
-        // Get the default meta tags from the config
-        $this->swiftypeMetaTags = Config::inst()->get(ContentExtension::class, 'swiftypeMetaTags');
+        // Get the namespace used as a prefix for all metaclasses
+        $namespace = $this->config()->get('namespace');
+        if ((!empty($namespace) && (substr($namespace, -1) != '\\'))) {
+            $this->namespace = $namespace . "\\";
+        } else {
+            $this->namespace = $namespace;
+        }
+        // Get the meta tag default classes from the config
+        $this->metaClasses = $this->config()->get('metaClasses');
     }
 
     public function getSwiftypeMetaTags()
     {
-        $swiftypeMetaTags = array();
+        $metaTags = array();
 
-        foreach ($this->swiftypeMetaTags as $tagName) {
-            $className = 'SwiftypeMetaTag_' . $tagName;
+        foreach ($this->metaClasses as $tagClass) {
+            $className = $this->namespace . $tagClass;
 
             if (!class_exists($className)) {
                 continue;
             }
 
-            /** @var SwiftypeMetaTag $r */
+            /**
+             * @var SwiftypeMetaTag $r
+             */
             $r = new $className();
-            $tagsString = $r->getMetaTagsString($this->data());
+            $tagsString = $r->getMetaTagsString($this->owner->data());
 
             if ($tagsString === null) {
                 continue;
             }
 
-            $swiftypeMetaTags[] = $tagsString;
+            $metaTags[] = $tagsString;
         }
 
-        return implode("\r\n", $swiftypeMetaTags);
+        return implode("\r\n", $metaTags);
     }
 }

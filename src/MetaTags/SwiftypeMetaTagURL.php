117bfec412c9c5fc67999240765f0a7e17fee42d
Mikaela Young <mikaela.young18@gmail.com>
2019-08-26 17:18:07 +1200

    Add URL meta tag back

---
+++ src/MetaTags/SwiftypeMetaTagURL.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ichaber\SSSwiftype\MetaTags;
+
+/**
+ * Class SwiftypeMetaTagURL
+ *
+ * @package Ichaber\SSSwiftype\MetaTags
+ */
+class SwiftypeMetaTagURL extends SwiftypeMetaTag
+{
+    /**
+     * @var string
+     */
+    protected $name = 'url';
+
+    /**
+     * @var string
+     */
+    protected $fieldName = 'AbsoluteLink';
+
+    /**
+     * @var string
+     */
+    protected $fieldType = 'enum';
+}

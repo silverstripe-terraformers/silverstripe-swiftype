9eb4559980ac25a45df16c2fd4c508cd7ca2e07b
Jared Dreyer <56658401+jareddreyerss@users.noreply.github.com>
2023-06-30 08:49:21 +1200

    Feature: Reindex crawler for files (#28)
    Co-authored-by: Cameron Bryers <12287346+Cambis@users.noreply.github.com>
    Co-authored-by: Chris Penny <chris.penny@gmail.com>

---
+++ src/Extensions/AbstractSwiftypeCrawlerExtension.php
@@ -0,0 +1,292 @@
+<?php
+
+namespace Ichaber\SSSwiftype\Extensions;
+
+use Ichaber\SSSwiftype\Service\SwiftypeCrawler;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Versioned\Versioned;
+
+/**
+ * @method DataObject|$this getOwner()
+ */
+abstract class AbstractSwiftypeCrawlerExtension extends DataExtension
+{
+    /**
+     * Urls to crawl
+     *
+     * array keyed by getOwnerKey
+     */
+    private array $urlsToCrawl = [];
+
+    public function setUrlsToCrawl(array $urls)
+    {
+        $this->urlsToCrawl = $urls;
+    }
+
+    public function getUrlsToCrawl(): array
+    {
+        return $this->urlsToCrawl;
+    }
+
+    /**
+     * We need to collate Urls before we write, just in case an author has changed the File's name (URL). If they
+     * have, then we need to request Swiftype to reindex both the old Url (which should then be marked by Swiftype
+     * as a 404), and the new Url
+     */
+    public function onBeforeWrite(): void
+    {
+        $this->collateUrls();
+    }
+
+    /**
+     * After a publish has occurred, we can collate and process immediately (no need to split things out like during
+     * an unpublish)
+     */
+    public function onAfterPublish(&$original): void
+    {
+        $this->collateUrls();
+        $this->processCollatedUrls();
+
+        // Check to see if the clearing of cache has been disabled (useful for unit testing, or any other reason you
+        // might have to disable it)
+        $clearCacheDisabled = Config::inst()->get(static::class, 'clear_cache_disabled');
+
+        if ($clearCacheDisabled) {
+            return;
+        }
+
+        // It's important that we clear the cache after we have finished requesting reindex from Swiftype
+        $this->clearCacheSingle();
+    }
+
+    /**
+     * We need to collate the Urls to be purged *before* we complete the unpublish action (otherwise, the LIVE Urls
+     * will no longer be available, since the page is now unpublished)
+     */
+    public function onBeforeUnpublish(): void
+    {
+        $this->collateUrls();
+    }
+
+    /**
+     * After the unpublish has completed, we can now request Swiftype to reindex the Urls that we collated
+     */
+    public function onAfterUnpublish(): void
+    {
+        $this->processCollatedUrls();
+
+        // Check to see if the clearing of cache has been disabled (useful for unit testing, or any other reason you
+        // might have to disable it)
+        $clearCacheDisabled = Config::inst()->get(static::class, 'clear_cache_disabled');
+
+        if ($clearCacheDisabled) {
+            return;
+        }
+
+        // It's important that we clear the cache after we have finished requesting reindex from Swiftype
+        $this->clearCacheSingle();
+    }
+
+    /**
+     * You may need to clear the cache at some point during your particular process
+     *
+     * Reset all Urls for any/all objects that might be in the cache (keeping in mind that Extensions are singleton,
+     * so the UrlsToCache could be accessed via singleton and it could contain Urls for many owner objects)
+     *
+     * We don't use flushCache (which is called from DataObject) because this is called between write and un/publish,
+     * and we need our cache to persist through these states
+     */
+    public function clearCacheAll(): void
+    {
+        $this->setUrlsToCrawl([]);
+    }
+
+    /**
+     * You may need to clear the cache at some point during your particular process
+     *
+     * Reset only the Urls related to this particular owner object (keeping in mind that Extensions are singleton,
+     * so the UrlsToCache could be accessed via singleton and it could contain Urls for many owner objects)
+     *
+     * We don't use flushCache (which is called from DataObject) because this is called between write and un/publish,
+     * and we need our cache to persist through these states
+     */
+    public function clearCacheSingle(): void
+    {
+        $urls = $this->getUrlsToCrawl();
+        $key = $this->getOwnerKey();
+
+        // Nothing for us to do here
+        if ($key === null) {
+            return;
+        }
+
+        // Nothing for us to do here
+        if (!array_key_exists($key, $urls)) {
+            return;
+        }
+
+        // Remove this key and it's Urls
+        unset($urls[$key]);
+
+        $this->setUrlsToCrawl($urls);
+    }
+
+    /**
+     * Collate Urls to crawl
+     *
+     * Extensions are singleton, so we use the owner key to make sure that we're only processing Urls directly related
+     * to the desired record
+     *
+     * You might need to collate more than one URL per Page (maybe you're using Fluent or another translation module).
+     * This is the method you will want to override in order to add that additional logic
+     */
+    public function collateUrls(): void
+    {
+        if (!$this->recordCanBeIndexed()) {
+            return;
+        }
+
+        // Grab any existing Urls so that we can add to it
+        $urls = $this->getUrlsToCrawl();
+
+        // Set us to a LIVE stage/reading_mode
+        $this->withVersionContext(function () use (&$urls) {
+            $key = $this->getOwnerKey();
+
+            // We can't do anything if we don't have a key to use
+            if ($key === null) {
+                return;
+            }
+
+            // Create a new container for this key
+            if (!array_key_exists($key, $urls)) {
+                $urls[$key] = [];
+            }
+
+            // Grab the absolute live link without ?stage=Live appended
+            $link = $this->getOwnerLink();
+
+            // If this record is not published, or we're unable to get a "Live Link" (for whatever reason), then there
+            // is nothing more we can do here
+            if (!$link) {
+                return;
+            }
+
+            // Nothing for us to do here, the Link is already being tracked
+            if (in_array($link, $urls[$key])) {
+                return;
+            }
+
+            // Add our base URL to this key
+            $urls[$key][] = $link;
+        });
+
+        // Update the Urls we have stored for indexing
+        $this->setUrlsToCrawl($urls);
+    }
+
+    /**
+     * Send requests to Swiftype to reindex each of the Urls that we have previously collated
+     */
+    protected function processCollatedUrls(): void
+    {
+        // Fetch the Urls that we need to reindex
+        $key = $this->getOwnerKey();
+
+        // We can't do anything if we don't have a key to process
+        if ($key === null) {
+            return;
+        }
+
+        $urls = $this->getUrlsToCrawl();
+
+        // There is nothing for us to do here if there are no Urls
+        if (count(array_keys($urls)) === 0) {
+            return;
+        }
+
+        // There are no Urls for this particular key
+        if (!array_key_exists($key, $urls)) {
+            return;
+        }
+
+        // Force the reindexing of each URL we collated
+        foreach ($urls[$key] as $url) {
+            $this->forceSwiftypeIndex($url);
+        }
+    }
+
+    protected function forceSwiftypeIndex(string $updateUrl): bool
+    {
+        // We don't reindex dev environments
+        if (Director::isDev()) {
+            return true;
+        }
+
+        $crawler = SwiftypeCrawler::create();
+
+        return $crawler->send($updateUrl);
+    }
+
+    protected function getOwnerKey(): ?string
+    {
+        $owner = $this->getOwner();
+
+        // Can't generate a key if the owner has not yet been written to the DB
+        if (!$owner->isInDB()) {
+            return null;
+        }
+
+        $key = str_replace('\\', '', $owner->ClassName . $owner->ID);
+
+        return $key;
+    }
+
+    /**
+     * Return the absolute link to the record, if the record is versioned return the live link
+     */
+    abstract protected function getOwnerLink(): ?string;
+
+    /**
+     * Return true if a record can be indexed
+     */
+    abstract protected function recordCanBeIndexed(): bool;
+
+    /**
+     * Sets the version context to Live as that's what crawlers will (normally) see
+     *
+     * The main function is to suppress the ?stage=Live querystring. LeftAndMain will set the default
+     * reading mode to 'DRAFT' when initialising so to counter this we need to re-set the default
+     * reading mode back to LIVE
+     */
+    private function withVersionContext(callable $callback): void
+    {
+        Versioned::withVersionedMode(static function () use ($callback) {
+            // Grab our current stage and reading mode
+            $originalDefaultReadingMode = Versioned::get_default_reading_mode();
+            $originalReadingMode = Versioned::get_reading_mode();
+            $originalStage = Versioned::get_stage();
+
+            // Set our stage and reading mode to LIVE
+            Versioned::set_default_reading_mode('Stage.' . Versioned::LIVE);
+            Versioned::set_reading_mode('Stage.' . Versioned::LIVE);
+            Versioned::set_stage(Versioned::LIVE);
+
+            // Process whatever callback was provided
+            $callback();
+
+            // Set us back to the original stage and reading mode
+            if ($originalReadingMode) {
+                Versioned::set_default_reading_mode($originalDefaultReadingMode);
+                Versioned::set_reading_mode($originalReadingMode);
+            }
+
+            if ($originalStage) {
+                Versioned::set_stage($originalStage);
+            }
+        });
+    }
+}
---
+++ tests/Extensions/SwiftypeFileCrawlerExtensionTest.php
@@ -0,0 +1,288 @@
+<?php
+
+namespace Ichaber\SSSwiftype\Tests\Extensions;
+
+use Exception;
+use Ichaber\SSSwiftype\Extensions\SwiftypeFileCrawlerExtension;
+use Ichaber\SSSwiftype\Tests\Fake\SwiftypeFile;
+use SilverStripe\Assets\Dev\TestAssetStore;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Versioned\Versioned;
+
+class SwiftypeFileCrawlerExtensionTest extends SapphireTest
+{
+    /**
+     * @var string
+     */
+    protected static $fixture_file = 'SwiftypeFileCrawlerExtensionTest.yml';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // File tests should be run in draft stage
+        Versioned::set_stage(Versioned::DRAFT);
+
+        // Make sure that our cache is cleared between tests
+        /** @var SwiftypeFileCrawlerExtension $crawlerExtension */
+        $crawlerExtension = Injector::inst()->get(SwiftypeFileCrawlerExtension::class);
+        $crawlerExtension->clearCacheAll();
+
+        // Set our config to not clear caches after un/publish, so that we can easily fetch the Urls for our test
+        Config::inst()->update(
+            SwiftypeFileCrawlerExtension::class,
+            'clear_cache_disabled',
+            true
+        );
+
+        // Set backend assets store root to /SwiftypeFileCrawlerExtensionTest
+        TestAssetStore::activate('SwiftypeFileCrawlerExtensionTest');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUrlsToCrawlPublished(): void
+    {
+        /** @var SwiftypeFile $file */
+        $file = $this->objFromFixture(SwiftypeFile::class, 'file_pdf');
+        $sourcePath = __DIR__ . '/../Fixtures/' . $file->Name;
+        $file->setFromLocalFile($sourcePath, $file->Filename);
+
+        // check our urls are not populated before we publish.
+        $urls = [];
+        $this->assertEquals($urls, $file->getUrlsToCrawl());
+
+        // Publish single so that Urls to crawl is populated
+        $file->publishSingle();
+
+        $expectedUrls = [
+            'localhost/assets/SwiftypeFileCrawlerExtensionTest/dummy.pdf',
+        ];
+
+        // Grab the Urls that we expect to have been collated
+        $key = str_replace('\\', '', $file->ClassName . $file->ID);
+        $urlsToCrawl = $file->getUrlsToCrawl();
+
+        // Check that the key exists for our file
+        $this->assertArrayHasKey($key, $urlsToCrawl);
+
+        // Grab the Urls that are for our page
+        $urlsToCrawl = $urlsToCrawl[$key];
+
+        // Strip out any http/https stuff
+        foreach ($urlsToCrawl as $urlToCrawl) {
+            $url = str_replace('http://', '', $urlToCrawl);
+            $url = str_replace('https://', '', $url);
+
+            $urls[] = $url;
+        }
+
+        $this->assertEquals($expectedUrls, $urls, '', 0.0, 10, true);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUrlsToCrawlUnPublished(): void
+    {
+        /** @var SwiftypeFile $file */
+        $file = $this->objFromFixture(SwiftypeFile::class, 'file_pdf');
+        $sourcePath = __DIR__ . '/../Fixtures/' . $file->Name;
+        $file->setFromLocalFile($sourcePath, $file->Filename);
+
+        // check our urls are not populated before we publish.
+        $urls = [];
+        $this->assertEquals($urls, $file->getUrlsToCrawl());
+
+        // Make sure our file is published before we begin
+        $file->publishSingle();
+
+        // Make sure we don't have any Cache set from the above publishing
+        $file->clearCacheAll();
+
+        // now we call the service to remove this file from index
+        $file->doUnpublish();
+
+        $expectedUrls = [
+            'localhost/assets/SwiftypeFileCrawlerExtensionTest/dummy.pdf',
+        ];
+
+        // Grab the Urls that we expect to have been collated
+        $key = str_replace('\\', '', $file->ClassName . $file->ID);
+        $urlsToCrawl = $file->getUrlsToCrawl();
+
+        // Check that the key exists for our page
+        $this->assertArrayHasKey($key, $urlsToCrawl);
+
+        // Grab the Urls that are for our page
+        $urlsToCrawl = $urlsToCrawl[$key];
+
+        // Strip out any http/https stuff
+        foreach ($urlsToCrawl as $urlToCrawl) {
+            $url = str_replace('http://', '', $urlToCrawl);
+            $url = str_replace('https://', '', $url);
+
+            $urls[] = $url;
+        }
+
+        $this->assertEquals($expectedUrls, $urls, '', 0.0, 10, true);
+    }
+
+    /**
+     * Files that hold no text (e.g. images) should not be indexed
+     *
+     * @throws Exception
+     */
+    public function testUrlsNotToCrawlPublished(): void
+    {
+        /** @var SwiftypeFile $file */
+        $file = $this->objFromFixture(SwiftypeFile::class, 'file_jpg');
+        $sourcePath = __DIR__ . '/../Fixtures/' . $file->Name;
+        $file->setFromLocalFile($sourcePath, $file->Filename);
+
+        // check our urls are not populated before we publish.
+        $urls = [];
+        $this->assertEquals($urls, $file->getUrlsToCrawl());
+
+        // Publish single so that Urls to crawl is populated
+        $file->publishSingle();
+
+        // Grab the Urls that we expect to have been collated
+        $this->assertEquals($urls, $file->getUrlsToCrawl());
+    }
+
+    /**
+     * Files that hold no text (e.g. images) should not be indexed
+     *
+     * @throws Exception
+     */
+    public function testUrlsNotToCrawlUnpublished(): void
+    {
+        /** @var SwiftypeFile $file */
+        $file = $this->objFromFixture(SwiftypeFile::class, 'file_jpg');
+        $sourcePath = __DIR__ . '/../Fixtures/' . $file->Name;
+        $file->setFromLocalFile($sourcePath, $file->Filename);
+
+        // check our urls are not populated before we publish.
+        $urls = [];
+        $this->assertEquals($urls, $file->getUrlsToCrawl());
+
+        // Make sure our file is published before we begin
+        $file->publishSingle();
+
+        // Make sure we don't have any Cache set from the above publishing
+        $file->clearCacheAll();
+
+        // now we call the service to remove this file from index
+        $file->doUnpublish();
+
+        // Grab the Urls that we expect to have been collated
+        $urlsToCrawl = $file->getUrlsToCrawl();
+        $this->assertEquals($urlsToCrawl, $file->getUrlsToCrawl());
+
+        // Check that the key does not exist for our File
+        $key = str_replace('\\', '', $file->ClassName . $file->ID);
+        $this->assertArrayNotHasKey($key, $urlsToCrawl);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUrlsToCrawlSegmentChanged(): void
+    {
+
+        /** @var SwiftypeFile $file */
+        $file = $this->objFromFixture(SwiftypeFile::class, 'file_pdf');
+        $sourcePath = __DIR__ . '/../Fixtures/' . $file->Name;
+        $file->setFromLocalFile($sourcePath, $file->Filename);
+
+        // check our urls are not populated before we publish anything.
+        $urls = [];
+        $this->assertEquals($urls, $file->getUrlsToCrawl());
+
+        // Make sure our file is published before we begin
+        $file->publishSingle();
+        $key = str_replace('\\', '', $file->ClassName . $file->ID);
+
+        // Make sure our cache is flushed from the above publishing
+        $file->clearCacheAll();
+
+        /**
+         * Note:
+         *  Somehow because File DBObject records in Stage.Stage create a hash path in the URL to access the file,
+         *  the asserted array contained 3 elements instead of 2. To ignore this we only asserted
+         *  that our old and new file URL's exist in the array that is sent for reindexing.
+         */
+        // Update our URL Segment
+        $file->renameFile('dummy-new.pdf');
+
+        // publish our file again to get new URL.
+        $file->publishSingle();
+
+        // We expect two URL's now. One from before the file rename change, and one from after it
+        $expectedUrls = [
+            'localhost/assets/SwiftypeFileCrawlerExtensionTest/dummy.pdf',
+            'localhost/assets/SwiftypeFileCrawlerExtensionTest/dummy-new.pdf',
+        ];
+
+        // Grab the Urls that we expect to have been collated
+        $urlsToCrawl = $file->getUrlsToCrawl();
+
+        // Check that the key exists for our page
+        $this->assertArrayHasKey($key, $urlsToCrawl);
+
+        // Grab the Urls that are for our file
+        $urlsToCrawl = $urlsToCrawl[$key];
+
+        // Strip out any http/https stuff
+        foreach ($urlsToCrawl as $urlToCrawl) {
+            $url = str_replace('http://', '', $urlToCrawl);
+            $url = str_replace('https://', '', $url);
+
+            $urls[] = $url;
+        }
+
+        // Assert that the $expectedUrls is a subset of $urls
+        $assertion = array_intersect($expectedUrls, $urls) === $expectedUrls;
+        $this->assertTrue($assertion);
+    }
+
+    public function testUrlsToCrawlCacheCleared(): void
+    {
+        // Since asserting cache is cleared we want to reenable cache for this test.
+        Config::inst()->update(
+            SwiftypeFileCrawlerExtension::class,
+            'clear_cache_disabled',
+            false
+        );
+
+        /** @var SwiftypeFile $file */
+        $file = $this->objFromFixture(SwiftypeFile::class, 'file_pdf');
+        $sourcePath = __DIR__ . '/../Fixtures/' . $file->Name;
+        $file->setFromLocalFile($sourcePath, $file->Filename);
+
+        // check our urls are not populated before we publish anything.
+        $urls = [];
+        $this->assertEquals($urls, $file->getUrlsToCrawl());
+
+        // Publish single so that Urls to crawl is populated
+        $file->publishSingle();
+
+        $key = str_replace('\\', '', $file->ClassName . $file->ID);
+
+        // Grab the Urls that we expect to have been collated
+        $urlsToCrawl = $file->getUrlsToCrawl();
+
+        // Check that the key exists for our page
+        $this->assertArrayNotHasKey($key, $urlsToCrawl);
+    }
+
+    public function tearDown(): void
+    {
+        TestAssetStore::reset();
+        parent::tearDown();
+    }
+}
